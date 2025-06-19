68c57ea5e81d33546394a7fa32e1b672feeee0a8
Daniel Danner <daniel@danner.de>
2025-06-19 15:41:23 +0200

    Make restic_json() log repo name

--- restic-health.py
+++ restic-health.py
@@ -71,14 +71,14 @@ for location_name, location in config_yaml['locations'].items():
             password_file = location['password_file'],
             backends = backends)
 
-async def restic_json(backend: BackendConfig, password_file: str, args: list[str]) -> str:
+async def restic_json(backend: BackendConfig, location: LocationConfig, args: list[str]) -> str:
     cache_dir_args = []
     if 'defaults' in config_yaml and 'cache_dir' in config_yaml['defaults']:
         cache_dir_args = ['--cache-dir', config_yaml['defaults']['cache_dir']]
 
     env = {
             'RESTIC_REPOSITORY': backend.repository,
-            'RESTIC_PASSWORD_FILE': password_file,
+            'RESTIC_PASSWORD_FILE': location.password_file,
             }
     cmd = ['restic', '--json', '--quiet', '--no-lock'] + cache_dir_args + args
 
@@ -93,24 +93,25 @@ async def restic_json(backend: BackendConfig, password_file: str, args: list[str
     stderr = bytes.decode(stderr_bytes, 'utf8')
 
     if proc.returncode != 0:
-        logging.error(f'Command {cmd} returned non-zero exit status {proc.returncode}. Standard error:\n{stderr.strip()}')
-        raise ResticHealthError(repo)
+        repo = f'{location.name}@{backend.name}'
+        logging.error(f'{repo}: Restic command {args} returned non-zero exit status {proc.returncode}. Standard error:\n{stderr.strip()}')
+        raise ResticHealthError()
 
     return stdout
 
 async def get_snapshots(location: LocationConfig, backend: BackendConfig) -> str:
-    stdout = await restic_json(backend, location.password_file, ['snapshots'])
+    stdout = await restic_json(backend, location, ['snapshots'])
     return stdout
 
 async def get_stats(location: LocationConfig, backend: BackendConfig, mode: str, snapshot: str|None = None) -> str:
     args = ['stats', '--mode', mode]
     if snapshot is not None:
         args.append(snapshot)
-    stdout = await restic_json(backend, location.password_file, args)
+    stdout = await restic_json(backend, location, args)
     return stdout
 
 async def get_diff_stats(location: LocationConfig, backend: BackendConfig, snapshot_ids: list[str]) -> str:
-    stdout = await restic_json(backend, location.password_file, ['diff'] + snapshot_ids)
+    stdout = await restic_json(backend, location, ['diff'] + snapshot_ids)
     lastline = stdout.splitlines()[-1]
     return lastline
 
@@ -130,7 +131,7 @@ async def write_state_file(location: LocationConfig, backend: BackendConfig, cat
     latest_link.symlink_to(file_path)
 
 async def get_locks(location, backend):
-    stdout = await restic_json(backend, location.password_file, ['list', 'locks'])
+    stdout = await restic_json(backend, location, ['list', 'locks'])
     return stdout.splitlines()
 
 async def get_latest_snapshot_timestamp(location, backend):
@@ -180,7 +181,7 @@ async def wait_until_unlocked(location, backend):
             if retries_remaining == 0:
                 logging.error(f'Giving up on {repo} (still locked) after dumping existing locks:')
                 for lock in locks:
-                    lock_content = await restic_json(backend, location.password_file, ['cat', 'lock', lock])
+                    lock_content = await restic_json(backend, location, ['cat', 'lock', lock])
                     print(lock_content)
                 raise ResticHealthError()
             logging.debug(f'{repo} is locked, waiting {retry_delay} seconds before retrying up to {retries_remaining} more time(s)')
